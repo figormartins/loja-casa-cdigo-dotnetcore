8b97815771fe1abdc19313fd264a8f1491fed00d
Igor Martins <figortmartins@gmail.com>
2019-12-21 23:23:02 -0300

    Gravando pedido na sessão

---
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(k => k.Id);
+
+            modelBuilder.Entity<Pedido>().HasKey(k => k.Id);
+            modelBuilder.Entity<Pedido>().HasMany(p => p.Itens).WithOne(p => p.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(p => p.Cadastro).WithOne(p => p.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(k => k.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(k => k.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(c => c.Pedido);
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Migrations/20191105214656_Model.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Migrations
+{
+    public partial class Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
